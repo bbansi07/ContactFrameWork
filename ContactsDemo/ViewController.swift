52e038a98144271e0f711e8f9ada5519aeef2fe2
Bansi Bhatt <bannsi.bhatt07@gmail.com>
2017-05-23 15:29:48 +0530

    Initial Commit

---
+++ ContactsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactsDemo
+//
+//  Created by Zeitech Solutions on 23/05/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
